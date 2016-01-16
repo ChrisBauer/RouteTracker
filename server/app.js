7ea0d277332ef816d1dc286075a6176a3bc6061b
chrisbauer <cab1113@gmail.com>
2016-01-16 16:44:27 -0500

    added server clustering to hopefully fix issues with express failing

--- server/app.js
+++ server/app.js
@@ -1,26 +1,50 @@
-var express = require('express');
-var path = require('path');
-var cors = require ('cors');
-var bodyParser = require('body-parser');
+var cluster = require('cluster');
 
-var routes = require('./routes/index');
+if (cluster.isMaster) {
+    var os = require('os');
 
-var app = express();
+    var cpuCount = os.cpus().length;
+    for (var i = 0; i < cpuCount; i++) {
+        cluster.fork();
+    }
 
-// view engine setup
-app.set('view engine', 'jade');
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+    cluster.on('exit', function (worker) {
+        console.log('worker %d exited, restarting',
+            worker.id);
+        cluster.fork();
+    });
+}
+else {
+    var express = require('express');
+    var path = require('path');
+    var cors = require('cors');
+    var bodyParser = require('body-parser');
 
-app.use('/', routes);
+    var routes = require('./routes/index');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+    var app = express();
 
-module.exports = app;
+    // view engine setup
+    app.set('view engine', 'jade');
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use('/', routes);
+
+    console.log('server %d running', cluster.worker.id);
+
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+    
+    app.listen(3000);
+    
+    module.exports = app;
+}
+
+
